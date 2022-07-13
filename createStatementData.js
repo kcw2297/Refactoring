4517b13f3e1fa4ee20aae650a016312542d1e72d
kcw2297 <chaeugg@gmail.com>
2022-07-13 12:32:25 +0900

    조건부 함수를 클래스로 옮긴다

--- createStatementData.js
+++ createStatementData.js
@@ -8,10 +8,11 @@ export default function createStatementData(invoice, plays){
     return result;
 
     function enrichPerformance(aPerformance){
+        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
+        result.play = calculator.play;
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result
     }
 
@@ -30,33 +31,48 @@ export default function createStatementData(invoice, plays){
     }
 
     function amountFor(aPerformance){
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
+    }
+
+    function volumeCreditsFor(aPerformance) {
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
+    }
+}
+
+class PerformanceCalculator{
+    constructor(aPerformance, aPlay){
+        this.performance = aPerformance;
+        this.play = aPlay;
+    }
+
+    get amount() {
         let result = 0;
     
-        switch (aPerformance.play.type){
+        switch (this.play.type){
             case "tragedy":
                 result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
+                if (this.performance.audience > 30) {
+                    result += 1000 * (this.performance.audience - 30);
                 }
                 break;
             case "comedy":
                 result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
+                if (this.performance.audience > 20) {
+                    result += 10000 + 500 * (this.performance.audience - 20);
                 }
-                result += 300 * aPerformance.audience;
+                result += 300 * this.performance.audience;
                 break;
             default:
-                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
+                throw new Error(`알 수 없는 장르: ${this.play.type}`);
         }
         return result;
     }
 
-    function volumeCreditsFor(aPerformance) {
+    get volumeCredits(){
         let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ("comedy" == aPerformance.play.type) 
-            result += Math.floor(aPerformance.audience / 5);
+        result += Math.max(this.performance.audience - 30, 0);
+        if ("comedy" == this.play.type) 
+            result += Math.floor(this.performance.audience / 5);
         return result;
     }
 }
