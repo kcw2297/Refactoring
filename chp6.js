036c8646d0bf47efb3dfe23d67b1af21f46db27e
kcw2297 <chaeugg@gmail.com>
2022-07-14 00:26:36 +0900

    6.2 ex2 start

--- chp6.js
+++ chp6.js
@@ -38,7 +38,13 @@ function printOwing(invoice){
 함수 인라인하기
 */
 
-function rating(aDriver){
-    return aDriver.numberOfLateDeliveries ? 2 : 1;
+function reportLines(aCustomer){
+    const lines = [];
+    gatherCustomerData(lines, aCustomer);
+    return lines;
 }
 
+function gatherCustomerData(out, aCustomer){
+    out.push(["name",aCustomer.name]);
+    out.push(["location", aCustomer.location]);
+}
