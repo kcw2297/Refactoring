5bc02e8aee8fe27198549593942a9d75653e9e27
kcw2297 <chaeugg@gmail.com>
2022-07-14 06:04:36 +0900

    6.6 변수 캡슐화하기 defaultowner example start

--- chp6.js
+++ chp6.js
@@ -93,3 +93,11 @@ const newEnglanders = someCusomters.filter(c=> inNewEngland(c.address.state));
 
 
 
+/* 
+변수 캡슐화하기
+*/
+
+let defaultOwner = {firstName:"마틴", lastName:"파울러"};
+
+spaceship.owner = defaultOwner;
+defaultOwner = {firstName:"레베카", lastName:"파슨스"};
