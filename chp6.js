a803ff538d023513dbb381883dc38b17aed8a9a9
kcw2297 <chaeugg@gmail.com>
2022-07-14 01:07:46 +0900

    6.3 함수 추출하기 order example finish

--- chp6.js
+++ chp6.js
@@ -56,3 +56,19 @@ function price(order){
     return basePrice - quantityDiscount + shipping;
 }
 
+class Order {
+    constructor(aRecord){
+        this._data = aRecord;
+    }
+
+    get quantity() {return this._data.quantity;}
+    get itemPrice() {return this._data.itemPrice;}
+
+    get price(){
+        return this.basePrice - this.quantityDiscount + this.shipping;
+    }
+
+    get basePrice(){ return this.quantity * this.itemPrice;} 
+    get quantityDiscount(){ return Math.max(0, this.quantity - 500) * this.itemPrice * 0.05;}
+    get shipping(){ return this.basePrice * 0.1, 100;}
+}
