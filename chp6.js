9711b3c821aa54eae9de89b524652cb9bf40b79c
kcw2297 <chaeugg@gmail.com>
2022-07-14 07:04:16 +0900

    6.6 변수 캡슐화하기 defaultowner example finish

--- chp6.js
+++ chp6.js
@@ -97,7 +97,23 @@ const newEnglanders = someCusomters.filter(c=> inNewEngland(c.address.state));
 변수 캡슐화하기
 */
 
-let defaultOwner = {firstName:"마틴", lastName:"파울러"};
 
-spaceship.owner = defaultOwner;
-defaultOwner = {firstName:"레베카", lastName:"파슨스"};
+
+let defaultOwnerData = {firstName:"마틴", lastName:"파울러"};
+function defaultOwner() {return Object.assign({}, defaultOwnerData);}
+function setDefaultOwner(arg) {defaultOwnerData = arg;}
+
+
+
+let defaultOwnerData2 = {firstName:"마틴", lastName:"파울러"};
+function defaultOwner() {return new Person(defaultOwnerData2);}
+function setDefaultOwner(arg) {defaultOwnerData2 = arg;}
+
+class Person {
+    constructor(data){
+        this._lastName = data.lastName;
+        this._firstName = data.firstName;
+    }
+    get lastName() {return this._lastName}
+    get firstName() {return this._firstName}
+}
