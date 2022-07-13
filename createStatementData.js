91435c6aaa1e28372464362fbcbb09cc405618e7
kcw2297 <chaeugg@gmail.com>
2022-07-13 14:08:34 +0900

    조건부 함수 다형화

--- createStatementData.js
+++ createStatementData.js
@@ -8,7 +8,7 @@ export default function createStatementData(invoice, plays){
     return result;
 
     function enrichPerformance(aPerformance){
-        const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+        const calculator = new createPerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount;
@@ -39,6 +39,43 @@ export default function createStatementData(invoice, plays){
     }
 }
 
+function createPerformanceCalculator(aPerformance, aPlay){
+    switch(aPlay.type){
+        case "tragedy":
+            return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy":
+            return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error('알 수 없는 장르');
+    }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+    get amount(){
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+    get amount(){
+        let result = 30000;
+        if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+        }
+        result += 300 * this.performance.audience;
+        return result;
+    }
+
+    get volumeCredits(){
+        return super.volumeCredits +  Math.floor(this.performance.audience / 5);
+
+    }
+}
+
 class PerformanceCalculator{
     constructor(aPerformance, aPlay){
         this.performance = aPerformance;
@@ -46,33 +83,10 @@ class PerformanceCalculator{
     }
 
     get amount() {
-        let result = 0;
-    
-        switch (this.play.type){
-            case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                result = 30000;
-                if (this.performance.audience > 20) {
-                    result += 10000 + 500 * (this.performance.audience - 20);
-                }
-                result += 300 * this.performance.audience;
-                break;
-            default:
-                throw new Error(`알 수 없는 장르: ${this.play.type}`);
-        }
-        return result;
+        throw new Error('서브 클래스에서 처리하였습니다.');
     }
 
     get volumeCredits(){
-        let result = 0;
         result += Math.max(this.performance.audience - 30, 0);
-        if ("comedy" == this.play.type) 
-            result += Math.floor(this.performance.audience / 5);
-        return result;
     }
 }
