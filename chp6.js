5d1d7832955e6686f105c790b135d1ddc3053aab
kcw2297 <chaeugg@gmail.com>
2022-07-14 00:11:40 +0900

    6.1 함수추출하기 finish

--- chp6.js
+++ chp6.js
@@ -6,22 +6,31 @@
 function printOwing(invoice){
     
     printBanner();
-    printDetails();
+    printDetails(invoice);
+    recordDueDate(invoice);
 
-    let outstanding = 0;
-    for (const o of invoice.orders){
-        outstanding += o.amount;
+    const outstanding = calculateOutstanding(invoice);
+    
+
+    function calculateOutstanding(invoice){
+        let result = 0;
+        for (const o of invoice.orders){
+            result += o.amount;
+        }
+        return result;
     }
     
-    const today = Clock.today;
-    invoice.dueDate = new Date(today.getFullYear())
     
+    function recordDueDate(invoice){
+        const today = Clock.today;
+        invoice.dueDate = new Date(today.getFullYear());
+    }
     
     function printBanner(){
         console.log('고객채무');
     };
     
-    function printDetails(){
+    function printDetails(invoice){
         console.log(`고객명 ${invoice.customer}`);
     }
 }
