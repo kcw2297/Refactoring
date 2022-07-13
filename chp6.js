e9260281626943154067e4c66b874e075ca55755
kcw2297 <chaeugg@gmail.com>
2022-07-14 00:31:10 +0900

    6.2 함수 인라인, reportlines example finish

--- chp6.js
+++ chp6.js
@@ -40,11 +40,7 @@ function printOwing(invoice){
 
 function reportLines(aCustomer){
     const lines = [];
-    gatherCustomerData(lines, aCustomer);
-    return lines;
-}
-
-function gatherCustomerData(out, aCustomer){
     out.push(["name",aCustomer.name]);
     out.push(["location", aCustomer.location]);
+    return lines;
 }
