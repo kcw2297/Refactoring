dce0c1e22583398f89d9b5012eff51dae4282729
kcw2297 <chaeugg@gmail.com>
2022-07-15 23:30:04 +0900

    8.7 반복문 쪼개기 example finish

--- chp8.js
+++ chp8.js
@@ -59,11 +59,14 @@ function emitPhotoData(p){
 /* 
 반복문 쪼개기
 */
-let youngest = people[0] ? people[0].age : Infinity;
-let totalSalary = 0;
-for (const p of people){
-    if (p.age < youngest) youngest = p.age;
-    totalSalary += p.salary;
+
+function Youngest(people){
+    return Math.min(people.map(p=>p.age));;
 }
-return `최연소 ${youngest}, 총 급여: ${totalSalary}`;
+
+function TotalSalary(people){
+    return people.reduce((total, p) => total + p.salary, 0);
+}
+
+return `최연소 ${Youngest(people)}, 총 급여: ${TotalSalary(people)}`;
 
