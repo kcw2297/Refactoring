2235e50d01eb9a1074689efd7470d37c6b4eb51b
kcw2297 <chaeugg@gmail.com>
2022-07-17 00:07:34 +0900

    10.5 특이 케이스 추가하기 example1 start

--- chp10.js
+++ chp10.js
@@ -106,7 +106,7 @@ function voyageRisk(voyage){
     let result = 1;
     if (voyage.length > 4) result += 2;
     if (voyage.length > 8) result += voyage.length - 8;
-    if (["중국", "동인도"].includes(voyage.zone)) result += 4;
+    if (["중국", "동인도"].includes(voyage.zone)) result += 4; // 특수한 조건
     return Math.max(result, 0);
 }
 
@@ -114,7 +114,7 @@ function captainHistoryRisk(voyage, history){
     let result = 1;
     if (history.length < 5) result += 4;
     result += history.filter(v => v.profit < 0).length;
-    if (voyage.zone === "중국" && hasChina(history)) result -= 2;
+    if (voyage.zone === "중국" && hasChina(history)) result -= 2; // 특수한 조건
     return Math.max(result, 0);
 }
 
@@ -126,7 +126,7 @@ function voyageProfitFactor(voyage, history){
     let result = 2;
     if (voyage.zone === "중국") result += 1;
     if (voyage.zone === "동인도") result += 1;
-    if (voyage.zone === "중국" && hasChina(history)){
+    if (voyage.zone === "중국" && hasChina(history)){ // 특수한 조건
         result += 3;
         if (history.length > 10) result += 1;
         if (voyage.length > 12) result += 1;
@@ -138,3 +138,126 @@ function voyageProfitFactor(voyage, history){
     }
     return result;
 }
+
+class Rating{
+    constructor(voyage, history){
+        this.voyage = voyage;
+        this.history = history;
+    }
+
+    get value(){
+        const vpf = voyageProfitFactor(this.voyage, this.history);
+        const vr = voyageRisk(this.voyage);
+        const chr = captainHistoryRisk(this.voyage, this.history);
+        if (vpf * 3 > (vr + chr * 2)) return "A";
+        else return "B";
+    }
+
+    get voyageRisk(){
+        let result = 1;
+        if (this.voyage.length > 4) result += 2;
+        if (this.voyage.length > 8) result += this.voyage.length - 8;
+        return Math.max(result, 0);
+    }
+
+    get captainHistoryRisk(){
+        let result = 1;
+        if (this.history.length < 5) result += 4;
+        result += this.history.filter(v => v.profit < 0).length;
+        return Math.max(result, 0);
+    }
+
+    get voyageProfitFactor(){
+        let result = 2;
+        if (this.voyage.zone === "중국") result += 1;
+        if (this.voyage.zone === "동인도") result += 1;
+        result += this.voyageLengthFactor;
+        result += this.historyLengthFactor;
+        return result;
+    }
+
+    get hasChinaHistory(){
+        return this.history.some(v => "중국" === v.zone);
+    }
+
+    get voyageLengthFactor(){
+        return (this.voyage.length > 14) ? -1 : 0;
+    }
+
+    get historyLengthFactor(){
+        return (this.history.length > 8) ? 1 : 0;
+    }
+}
+
+
+class ExperiencedChinaRating extends Rating {
+    get voyageProfitFactor(){
+        return super.voyageProfitFactor + 3;
+    }
+
+    get voyageRisk(){
+        return super.voyageRisk + 4;
+    }
+
+    get captainHistoryRisk(){
+        const result = super.captainHistoryRisk - 2;
+        return Math.max(result,0);
+    }
+
+    get voyageLengthFactor(){
+        let result = 0;
+        if (this.voyage.length > 12) result += 1;
+        if (this.voyage.length > 18) result -= 1;
+        return result;
+    }
+
+    get historyLengthFactor(){
+        return (this.history.length > 10) ? 1 : 0;
+    }
+}
+
+function createRating(voyage, history){
+    if (this.voyage.zone === "중국" && this.hasChinaHistory){
+        return new ExperiencedChinaRating(voyage, history);
+    }
+    else return new Rating(voyage, history);
+}
+
+function rating(voyage, history){
+    return createRating(voyage, history).value;
+}
+
+
+/* 
+특이 케이스 추가하기
+*/
+
+class Site{
+    constructor(customer){
+        this._customer = customer;
+    }
+    get customer(){return this._customer;}
+}
+
+class Customer{
+    get name(){}
+    get billingPlan(){}
+    set billingPlan(arg){}
+    get paymentHistory(){}
+}
+
+//client 1
+const aCustomer = Site.customer;
+//
+let customerName;
+if (aCustomer === "미확인 고객") customerName = "거주자";
+else customerName = aCustomer.name;
+
+// client2
+const plan = (aCustomer === "미확인 고객") ? registry.billingPlans.basic : aCustomer.billingPlan;
+
+//client3
+if (aCustomer !== "미확인 고객") aCustomer.billingPlan = newPlan;
+
+//client4
+const weeksDelinquent = (aCustomer === "미확인 고객") ? 0 : aCustomer.paymentHistory.weeksDelinquentInLastYear;
