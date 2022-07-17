97d40906e4a9f420885a843a5e03d857b930c4b9
kcw2297 <chaeugg@gmail.com>
2022-07-17 18:43:37 +0900

    11.9 함수를 명령으로 바꾸기 example start

--- chp11.js
+++ chp11.js
@@ -3,16 +3,62 @@
 */
 
 function alertForMiscreant(people){
+    findMiscreant(people) ? setOffAlarms() : ";"
+}
+
+
+function findMiscreant(people){
     for (const p of people){
         if (p === "조커"){
-            setOffAlarms();
             return "조커";
         }
         if (p === "사루만"){
-            setOffAlarms();
             return "사루만";
         }
     }
     return "";
 }
 
+
+/*
+생성자를 팩터리 함수로 바꾸기
+*/
+
+class Employee{
+    constructor(name, typeCode){
+        this._name = name;
+        this._typeCode = typeCode;
+    }
+    get name(){return this._name;}
+    get type(){return Employee.legalTypeCodes[this._typeCode];}
+    static get legalTypeCodes(){
+        return {"E":"Engineer", "M":"Manager","S":"Salesperson"};
+    }
+}
+
+function createEngineer(name){
+    return new Employee(name, "E")
+}
+
+
+/*
+함수를 명령으로 바꾸기
+*/
+
+function score(candidate, medicalExam, scoringGuide){
+    let result = 0;
+    let healthLevel = 0;
+    let highMedicalRiskFlag = false;
+
+    if (medicalExam.isSmoker){
+        healthLevel += 10;
+        highMedicalRiskFlag = true;
+    }
+    let certificationGrade = "regular";
+    if (scoringGuide.stateWithLowCertification(candidate.originState)){
+        certificationGrade = "low";
+        result -= 5;
+    }
+    result -= Math.max(healthLevel - 5, 0);
+    return result;
+}
