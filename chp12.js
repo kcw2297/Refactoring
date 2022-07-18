9d63ce7bf10f0084355082f2fa013a2b11600417
kcw2297 <chaeugg@gmail.com>
2022-07-18 11:22:04 +0900

    12.10 서브클래스를 위임으로 바꾸기 example1 finish

--- chp12.js
+++ chp12.js
@@ -74,24 +74,51 @@ class Booking{
         return this._show.hasOwnProperty('talkback') && !this.isPeakDay;
     }
     get basePrice(){
+        return (this._premiumDelegate) ? this._premiumDelegate.basePrice : this._privateBasePrice
+    }
+
+    get _privateBasePrice(){
         let result = this._show.price;
         if (this.isPeakDay) result += Math.round(result * 0.15);
         return result;
     }
+    get hasDinner(){
+        return (this._premiumDelegate) ? this._premiumDelegate.hasDinner : undefined;
+    }
+
+    _bePremium(extras){
+        this._premiumDelegate = new PremiumBookingDelegate(this, extras);
+    }
 }
 
-class PremiumBooking{
-    constructor(show,date,extras){
-        super(show, date);
+
+class PremiumBookingDelegate{
+    constructor(hostBooking, extras){
+        this._host = hostBooking;
         this._extras = extras;
     }
     get hasTalkback(){
-        return this._show.hasOwnProperty('talkback');
+        return this._host._show.hasOwnProperty('talkback');
     }
     get basePrice(){
-        return Math.round(super.basePrice + this._extras.premiumFee);
+        return Math.round(this._host._privateBasePrice + this._extras.premiumFee);
     }
     get hasDinner(){
-        return this._extras.hasOwnProperty('dinner') && !this.isPeakDay;
+        return this._extras.hasOwnProperty('dinner') && !this._host.isPeakDay;
     }
 }
+
+
+function createBooking(show, date){
+    return new Booking(show, date);
+}
+
+function createPremiumBooking(show, date, extras){
+    result =  new Booking(show, date, extras);
+    result._bePremium(extras);
+    return result;
+}
+
+aBooking = createBooking(show, date);
+aBooking2 = createPremiumBooking(show, date, extras);
+
